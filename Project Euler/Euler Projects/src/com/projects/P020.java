e685fddfa199369523759d5abbe85ace3a549eb8
SatyaDivya <divya.maddipudi1@gmail.com>
2020-09-28 20:41:18 +0530

    Modified P020

--- Project Euler/Euler Projects/src/com/projects/P020.java
+++ Project Euler/Euler Projects/src/com/projects/P020.java
@@ -13,20 +13,21 @@ package com.projects;
 import java.math.BigInteger;
 
 public class P020 {
-	
+
 	public static void main(String[] args) {
-		//System.out.println(factorial(100));
-		//BigInteger value = new BigInteger(String.valueOf(factorial(100)));
+		// System.out.println(factorial(100));
+		// BigInteger value = new BigInteger(String.valueOf(factorial(100)));
 		String value = String.valueOf(factorial(100));
 		long sum = 0;
-		for(int i=0;i<value.length();i++) {
+		for (int i = 0; i < value.length(); i++) {
 			sum = sum + Character.getNumericValue(value.charAt(i));
 		}
 		System.out.println(sum);
 	}
+
 	public static BigInteger factorial(int num) {
 		BigInteger factorialValue = BigInteger.ONE;
-		for(int i=1;i<=num;i++) {
+		for (int i = 1; i <= num; i++) {
 			BigInteger val = new BigInteger(String.valueOf(i));
 			factorialValue = factorialValue.multiply(val);
 		}
