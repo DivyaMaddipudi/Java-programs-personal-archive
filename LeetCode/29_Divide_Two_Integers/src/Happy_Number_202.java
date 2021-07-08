1f5e56fe44a238053d299bcea9aecf554c9ac7b1
SatyaDivya <divya.maddipudi1@gmail.com>
2021-07-09 01:09:04 +0530

    Java program modified

--- LeetCode/29_Divide_Two_Integers/src/Happy_Number_202.java
+++ LeetCode/29_Divide_Two_Integers/src/Happy_Number_202.java
@@ -5,23 +5,16 @@ public class Happy_Number_202 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int value = sc.nextInt();
-
-
         int squareValue = 0;
 
-
         while(value > 9) {
-
             while (value >  0) {
                 int rem = value % 10;
                 squareValue = squareValue + (rem * rem);
                 value = value / 10;
             }
-
             value = squareValue;
-
             squareValue = 0;
-
         }
         if(value == 1) {
             System.out.println("Happy number");
---
+++ Project Euler/Euler Projects/src/com/projects/P026.java
@@ -0,0 +1,21 @@
+package com.projects;
+
+import java.util.HashSet;
+
+public class P026 {
+	
+	public static void main(String[] args) {
+		
+		
+		for(int i=1000;i>0;i--) {
+			int num = i;
+			while(num != 0) {
+				int rem = 1%num;
+				int value = 10 * rem;
+				
+			}
+		}
+		
+	}
+
+}
